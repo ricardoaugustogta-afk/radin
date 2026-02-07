a1b4c1b2b949649001c3685cae6445f6d98a45eb
ricardoaugustogta-afk <ricardoaugustogta@gmail.com>
2026-02-07 12:00:38 -0400

    Create build.gradle.kts file for the Radin project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,11 @@
 plugins { 
-    id("com.android.application") version "8.0.0" apply false 
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false 
+    kotlin("jvm") version "1.5.31" 
 }
 
-android {
-    compileSdk = 33
-
-    defaultConfig {
-        applicationId = "com.example.myapp"
-        minSdk = 21
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+repositories { 
+    mavenCentral() 
 }
 
-dependencies {
-    implementation("androidx.core:core-ktx:1.10.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    testImplementation("junit:junit:4.13.2")
+dependencies { 
+    implementation(kotlin("stdlib")) 
 }
